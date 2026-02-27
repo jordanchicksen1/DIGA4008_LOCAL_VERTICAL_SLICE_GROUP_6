24b410613a0b183115b940754e1386d9c2a028db
RamzCas <2749414@students.wits.ac.za>
2026-02-27 16:11:16 +0200

    Manager scripts added

---
+++ LOCAL_VERTICAL_SLICE/Assets/scripts/GameStateManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GameStateManager : MonoBehaviour
+{
+    [Header("Audio Container")]
+    public AudioSource[] audioSources;
+
+    [Header("Scoring")]
+    public float ItemsRevomed;
+    public float ItemsRemaining;
+
+
+    void Update()
+    {
+        
+    }
+}
